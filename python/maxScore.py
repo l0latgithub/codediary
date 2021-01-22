566c744bd754b9b8696c688484cf90ca31c61ebc
L Liu <tamu_lijunliu@yahoo.com>
2021-01-22 15:40:29 -0600

    Solution for Maximum Points You Can Obtain from Cards

---
+++ python/maxScore.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        
+        """
+        There are several cards arranged in a row, 
+        and each card has an associated number of points.
+        The points are given in the integer array cardPoints.
+        
+        In one step, you can take one card from the beginning 
+        or from the end of the row. You have to take exactly k cards.
+
+        Your score is the sum of the points of the cards you have taken.
+        Given the integer array cardPoints and the integer k, 
+        return the maximum score you can obtain.
+        """
+        
+        # runsum = sum(cardPoints[-k:])
+        # maxsum = runsum
+        # for i in range(k):
+        #     runsum +=cardPoints[i] - cardPoints[i-k]
+        #     maxsum = max(maxsum, runsum)
+        # return maxsum
+        runsum, maxsum = 0, 0
+        for idx, val in enumerate(cardPoints[-k:] + cardPoints[:k]):
+            runsum += val
+            
+            if idx>=k:
+                runsum -= cardPoints[-k+idx-k]
+            maxsum = max(maxsum, runsum)
+        return maxsum
